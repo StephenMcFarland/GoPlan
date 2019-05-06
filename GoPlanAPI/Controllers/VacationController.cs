9e0bebbf63f03f6dd3cb646c22756f181f1ee54c
stephenmcfarland <snmcfarland@gmail.com>
2019-05-06 13:10:16 -0400

    Post Get Put GetByUser

--- GoPlanAPI/Controllers/VacationController.cs
+++ GoPlanAPI/Controllers/VacationController.cs
@@ -13,26 +13,26 @@ namespace GoPlanAPI.Controllers
     [Authorize]
     public class VacationController : ApiController
     {
-        public IHttpActionResult CreateVacation(VacationCreate model)
+        public IHttpActionResult Post(VacationCreate model)
         {
             VacationService vacationService = CreateVacationService();
             var vacation = vacationService.CreateVacation(model);
             return Ok(vacation);
         }
-        public IHttpActionResult GetVacations()
+        public IHttpActionResult Get()
         {
             VacationService vacationService = CreateVacationService();
             var vacations = vacationService.GetVacations();
             return Ok(vacations);
         }
 
-        public IHttpActionResult GetVacationByUser()
+        public IHttpActionResult GetByUser()
         {
             VacationService vacationService = CreateVacationService();
             var vacation = vacationService.GetVacationByUser();
             return Ok(vacation);
         }
-        public IHttpActionResult UpdateVacation(VacationEdit model)
+        public IHttpActionResult Put(VacationEdit model)
         {
             VacationService vacationService = CreateVacationService();
             var vacations = vacationService.UpdateVacation(model);
