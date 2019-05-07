51e548c985b24912d802c68c0f0d79eaabb82e18
Logan <libinator@gmail.com>
2019-05-07 16:30:28 -0400

    Finishing Cors Setup

--- GoPlanAPI/Startup.cs
+++ GoPlanAPI/Startup.cs
@@ -12,6 +12,7 @@ namespace GoPlanAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
